74a7eb8e001c1e10aa8dc336c1287e2bb6d7bdbf
Nigel Smith <nigel.nds.smith@gmail.com>
2026-09-02 11:19:35 -0600

    updated version

--- internal/version/version.go
+++ internal/version/version.go
@@ -12,7 +12,7 @@
 package version
 
 // Version is the game's current release. Moon Miner is in alpha.
-const Version = "1.7.0"
+const Version = "1.7.1"
 
 // Channel is the human-readable release channel derived from Version's
 // leading component.
